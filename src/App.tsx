3f65f98b02759c83f15c2426e36ea829f267b37a
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2023-09-09 09:54:43 +0100

    Update app + packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { AddUser } from "./components/add-user";
+import { ViewUser } from "./components/view-user";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <>
+      <h1>React CRUD app</h1>
+      <AddUser input="Mussa" />
+      {/* <Button colorScheme="blue">Button</Button> */}
+      <ViewUser />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/view-user.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ViewUser = () => {
+  return (
+    <div>
+      <h1>View users</h1>
+    </div>
+  );
+};
