65aff79ae93c83b0a30132c7647e1457fd1bef5b
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2023-09-09 13:46:15 +0100

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,21 @@
+import { Badge } from "@chakra-ui/react";
+
+interface CriticScoreProps {
+  score: number;
+}
+
+const CriticScore = ({ score }: CriticScoreProps) => {
+  let color = score > 75 ? "green" : score > 16 ? "yellow" : "";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
